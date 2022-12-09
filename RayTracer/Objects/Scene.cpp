50b3ea7c310af2bba2ba80a922451adef53e1057
Wanable123 <98624940+Wanable123@users.noreply.github.com>
2022-12-08 17:24:47 -0700

    Add project files.

---
+++ RayTracer/Objects/Scene.cpp
@@ -0,0 +1,63 @@
+#include "Scene.h"
+
+#include "Objects.h"
+
+color3 Scene::Trace(const Ray& ray, float min, float max, RaycastHit& hit, int depth){
+
+    bool rayHit = false;
+
+    float closest = max;
+
+    // get closest ray hit
+    
+    for (auto& object : m_objects){
+
+        if (object->Hit(ray, min, closest, hit)){
+
+            rayHit = true;
+            
+            closest = hit.distance;
+        
+        }
+
+    }
+
+    // if ray hit object, scatter (bounce) ray and check for next hit
+
+    if (rayHit){
+
+        Ray scattered;
+        
+        color3 attenuation;
+
+        // limit the number of bounces to depth, get color attenuation
+        
+        // final color is product of object colors (object->material * object->material * ...)
+        
+        if (depth > 0 && hit.material->Scatter(ray, hit, attenuation, scattered)){
+
+            return attenuation * Trace(scattered, min, max, hit, depth - 1);
+
+        }else{
+
+            return hit.material->getEmissive();
+        
+        }
+
+    }else{
+
+        glm::vec3 direction = glm::normalize(ray.direction);
+        
+        float t = (direction.y + 1) * 0.5f;
+        
+        return lerp(m_topColor, m_bottomColor, t);
+    
+    }
+
+}
+
+void Scene::AddObject(std::unique_ptr<Object> object){
+
+    m_objects.push_back(std::move(object));
+
+}
---
+++ RayTracer/Renderer.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include"Renderer/Canvas.h"
+
+#include "Math/Ray.h"
+
+#include "Objects/Objects.h"
+
+#include "Renderer/Camera.h"
+
+#include<SDL.h>
+
+class Scene;
+
+class Renderer{
+
+public:
+
+	Renderer() = default;
+
+	bool Initialize();
+
+	void Shutdown();
+
+	bool CreateWindow(int width, int height);
+
+	void CopyCanvas(const Canvas& canvas);
+
+	void Render(Canvas& canvas, Scene& scene, Camera& camera, int samples = 5);
+
+	void Present();
+
+	friend class Canvas;
+
+private:
+
+	color3 GetBackgroundFromRay(const Ray& ray);
+
+	SDL_Renderer* m_renderer{ nullptr };
+
+	SDL_Window* m_window{ nullptr };
+
+};
